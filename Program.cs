af4578db9069e71b5ab9a5e4e4992f402e4b2fbb
Tatiana <tsmall@rambler.ru>
2023-03-15 18:30:29 +0600

    Добавили функцию создания нового массива

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿string[] array1 = new string[4] { "Татьяна", "Смольянинова", "жен", "зам" };
 string[] array2 = new string[array1.Length];
 
+void CopyArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
